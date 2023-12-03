c4cdd84c507c6e4980e1f9a8d317127b050de505
YaraGamal5 <yaraagamaal55@gmail.com>
2023-12-03 14:31:45 +0200

    first commit

---
+++ src/store/apis/albumsApi.js
@@ -0,0 +1,69 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
+
+//TODO: This function is for development purpose only.. delete this function at the end of development.
+const pause = async (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+};
+
+const albumsApi = createApi({
+    reducersPath: "albums",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:3005",
+        fetchFn: async (...args) => {
+            await pause(1000);
+            return fetch(...args);
+        }
+    }),
+
+    endpoints: (builder) => ({
+        removeAlbum: builder.mutation({
+            invalidatesTags: (result, error, album) => [{ type: 'Album', id: album.id }],
+            query: (album) => {
+               return {
+                url: `/albums/${album.id}`,
+                method: 'DELETE',
+            };
+        }
+    }),
+        addAlbum: builder.mutation({
+            invalidatesTags: (result, error, user) => [{ type: 'UsersAlbums', id: user.id }],
+            query: (user) => (
+                {
+                    url: '/albums',
+                    method: 'POST',
+                    body: {
+                        userId: user.id,
+                        title: faker.commerce.productName(),
+                    }
+                }
+            )
+        }),
+
+        fetchAlbums: builder.query({
+            providesTags: (result, error, user) => {
+                const tags = result.map((album) => {
+                    return { type: 'Album', id: album.id };
+                });
+                tags.push({ type: 'UsersAlbums', id: user.id });
+                return tags;
+            },
+            query: (user) => (
+                {
+                    url: '/albums',
+                    params: {
+                        userId: user.id
+                    },
+                    methods: 'GET'
+                })
+        })
+
+    }),
+
+});
+
+
+export const { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } = albumsApi;
+export { albumsApi };
---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+  const response = await axios.get("http://localhost:3005/users");
+
+  // DEV ONLY!!!
+  await pause(1000);
+
+  return response.data;
+});
+
+// When a thunk is created three properties associated with it 
+// fetchUsers.pending: 'users/fetch/pending'
+// fetchUsers.fulfilled: 'users/fetch/fulfilled'
+// fetchUsers.rejected: 'users/fetch/rejected'
+// These could be used in reducer. 
+
+// DEV ONLY!!!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+export { fetchUsers };
